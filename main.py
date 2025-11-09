df71f3656e73d8597a4dac095cb1593c5cc4d807
olyasha4U <lybo.km15@gmail.com>
2025-11-09 21:52:22 +0200

    3 commit

--- main.py
+++ main.py
@@ -36,13 +36,27 @@
 #
 # car=Car()
 # car.get_info()
-3
-class Employee:
-    def __init__(self, name="Alex", position="manager", salary=65000):
-        self.name=name
-        self.position=position
-        self.salary=salary
-    def get_salary_info(self):
-        print(f"Заробітна плата {self.name}: {self.salary}")
-employee=Employee()
-employee.get_salary_info()
+# 3
+# class Employee:
+#     def __init__(self, name="Alex", position="manager", salary=65000):
+#         self.name=name
+#         self.position=position
+#         self.salary=salary
+#     def get_salary_info(self):
+#         print(f"Заробітна плата {self.name}: {self.salary}")
+# employee=Employee()
+# employee.get_salary_info()
+4
+class Rectangle:
+    def __init__(self, width=4, height=6):
+        self.width=width
+        self.height=height
+    def calculate_area(self):
+        area=self.height*self.width
+        print(f"Rectangle area ={area}")
+    def calculate_perimeter(self):
+        perimeter=self.height+self.width
+        print(f"Rectangle perimeter={perimeter}")
+rec=Rectangle()
+rec.calculate_area()
+rec.calculate_perimeter()
