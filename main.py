d5e565d6f4077b8a89d8406725c3a59d80ac2b06
olyasha4U <lybo.km15@gmail.com>
2025-11-09 20:46:25 +0200

    2 commit

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
-1
-class BankAccount:
-    def __init__(self, account_number=0, balance=0):
-        self.account_number=account_number
-        self.balance=balance
+# 1
+# class BankAccount:
+#     def __init__(self, account_number=0, balance=0):
+#         self.account_number=account_number
+#         self.balance=balance
+#
+#     def deposit(self, money):
+#         self.balance += money
+#         print(f"На рахунок зараховано {money} грн. Поточний баланс: {self.balance} грн.")
+#
+#     def withdraw(self, money):
+#         if self.balance >= money:
+#             self.balance -= money
+#             print(f"З рахунку знято {money} грн. Залишок: {self.balance} грн.")
+#         else:
+#             print(f"Недостатньо коштів! Ваш баланс: {self.balance} грн.")
+# a1=1000
+# a2=200
+# a3=801
+# account=BankAccount()
+#
+# account.deposit(a1)
+# account.withdraw(a2)
+# account.withdraw(a3)
+# print(account)
+2
+class Car:
+    def __init__(self, make="Benz", model="Patent-Motorwagen", year=1885 ):
+        self.make=make
+        self.model=model
+        self.year=year
+        self.info=[]
+    def get_info(self):
+        print(f"Марка: {self.make}, Модель: {self.model}, Рік: {self.year}")
 
-    def deposit(self, money):
-        self.balance += money
-        print(f"На рахунок зараховано {money} грн. Поточний баланс: {self.balance} грн.")
 
-    def withdraw(self, money):
-        if self.balance >= money:
-            self.balance -= money
-            print(f"З рахунку знято {money} грн. Залишок: {self.balance} грн.")
-        else:
-            print(f"Недостатньо коштів! Ваш баланс: {self.balance} грн.")
-a1=1000
-a2=200
-a3=801
-account=BankAccount()
-
-account.deposit(a1)
-account.withdraw(a2)
-account.withdraw(a3)
-print(account)
+car=Car()
+car.get_info()
