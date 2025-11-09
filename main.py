5bc03472320e8653c0252166810d66d644639bb0
olyasha4U <lybo.km15@gmail.com>
2025-11-09 21:35:29 +0200

    3 commit

--- main.py
+++ main.py
@@ -23,16 +23,26 @@
 # account.withdraw(a2)
 # account.withdraw(a3)
 # print(account)
-2
-class Car:
-    def __init__(self, make="Benz", model="Patent-Motorwagen", year=1885 ):
-        self.make=make
-        self.model=model
-        self.year=year
-        self.info=[]
-    def get_info(self):
-        print(f"Марка: {self.make}, Модель: {self.model}, Рік: {self.year}")
-
-
-car=Car()
-car.get_info()
+# 2
+# class Car:
+#     def __init__(self, make="Benz", model="Patent-Motorwagen", year=1885 ):
+#         self.make=make
+#         self.model=model
+#         self.year=year
+#         self.info=[]
+#     def get_info(self):
+#         print(f"Марка: {self.make}, Модель: {self.model}, Рік: {self.year}")
+#
+#
+# car=Car()
+# car.get_info()
+3
+class Employee:
+    def __init__(self, name="Alex", position="manager", salary=65000):
+        self.name=name
+        self.position=position
+        self.salary=salary
+    def get_salary_info(self):
+        print(f"Заробітна плата {self.name}: {self.salary}")
+employee=Employee()
+employee.get_salary_info()
